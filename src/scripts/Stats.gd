1da591c2b4f0c1e4b27fe543b9f62a69659459e1
unknown <yvon.deplante@gmail.com>
2020-10-21 19:28:03 +0200

    Bug fix: stats reset correctly at new game

--- src/scripts/Stats.gd
+++ src/scripts/Stats.gd
@@ -16,13 +16,13 @@ var bonus_coins_lost := 0
 
 
 func reset() -> void:
-	var patterns_dodged := 0
-	var rains_dodged := 0
+	patterns_dodged = 0
+	rains_dodged = 0
 
-	var coins_caught := 0
-	var bonus_caught := 0
-	var bonus_coins_caught := 0
+	coins_caught = 0
+	bonus_caught = 0
+	bonus_coins_caught = 0
 
-	var coins_lost := 0
-	var bonus_lost := 0
-	var bonus_coins_lost := 0
+	coins_lost = 0
+	bonus_lost = 0
+	bonus_coins_lost = 0
