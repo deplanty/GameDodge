009572b5a7b86854700e9595195913674356637b
unknown <yvon.deplante@gmail.com>
2020-10-27 20:10:29 +0100

    Button to go back to main menu from mode selection menu

--- src/scripts/ModeSelection.gd
+++ src/scripts/ModeSelection.gd
@@ -1,25 +1,36 @@
 extends Control
 
 
+var next_scene := String()
+
+
 func _ready() -> void:
 	$FadeTransition.fade_out()
 
 
 func _on_NormalButton_pressed() -> void:
 	Globals.game_mode_selected = "GAME_MODE_NORMAL"
+	next_scene = "res://src/actors/Level.tscn"
 	$FadeTransition.fade_in()
 
 
 func _on_WTFButton_pressed() -> void:
 	Globals.game_mode_selected = "GAME_MODE_COINSFRENZY"
+	next_scene = "res://src/actors/Level.tscn"
 	$FadeTransition.fade_in()
 
 
 func _on_RainButton_pressed() -> void:
 	Globals.game_mode_selected = "GAME_MODE_RAIN"
+	next_scene = "res://src/actors/Level.tscn"
 	$FadeTransition.fade_in()
 
 
 func _on_FadeTransition_animation_finished(anim_name: String) -> void:
 	if anim_name == "fade_in":
-		get_tree().change_scene("res://src/actors/Level.tscn")
+		get_tree().change_scene(next_scene)
+
+
+func _on_BackButton_pressed() -> void:
+	next_scene = "res://src/actors/MainMenu.tscn"
+	$FadeTransition.fade_in()
