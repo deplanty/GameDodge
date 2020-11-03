c8b187ec75bdce1a2df7a04f73742c2d66b89c41
unknown <yvon.deplante@gmail.com>
2020-11-03 20:05:05 +0100

    Add two new patterns

--- src/scripts/Tool_Pattern.gd
+++ src/scripts/Tool_Pattern.gd
@@ -1,10 +1,15 @@
 extends Node2D
 
+const path_res := "res://assets/files/patterns.json"
+
 
 func _ready() -> void:
+	get_tree().paused = true
 	print("Processing patterns...")
 	var patterns := Array()
 	for i in $Patterns.get_child_count():
+		if not $Patterns.get_child(i).visible:
+			continue
 		print("Pattern: ", $Patterns.get_child(i).name)
 		print("\t", $Patterns.get_child(i).get_child_count(), " enemies")
 		var pattern := Array()
@@ -18,10 +23,15 @@ func _ready() -> void:
 	print("Done!")
 
 	var fid := File.new()
-	fid.open("res://assets/patterns.json", fid.WRITE)
+	fid.open(path_res, fid.WRITE)
 	fid.store_line(JSON.print(patterns, "\t"))
 	fid.close()
+	get_tree().paused = false
 
 
 func _on_QuitButton_pressed() -> void:
 	get_tree().quit()
+
+
+func _on_ReloadButton_pressed() -> void:
+	get_tree().reload_current_scene()
